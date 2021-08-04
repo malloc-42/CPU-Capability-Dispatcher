a82b1fe8a1bb47f81b0792fe922fd1e39a66b3c4
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-08-04 22:37:06 +0530

    use internal array

--- avx/impl.cpp
+++ avx/impl.cpp
@@ -1,16 +1,44 @@
 #include "interface.h"
 #include <x86intrin.h>
+#include <exception>
 
-namespace avx2{
-
-std::array<double, 4> mul(const std::array<double, 4> &x, const std::array<double, 4> &y)
+namespace avx2
 {
-    std::cout << "AVX IMPL\n";
-    auto x_vec = __m256d{x[0], x[1], x[2], x[3]};
-    auto y_vec = __m256d{y[0], y[1], y[2], y[3]};
-    auto mul = _mm256_mul_pd(x_vec, y_vec);
 
-    return {mul[0], mul[1], mul[2], mul[3]};
-}
+    array mul(const array &x_arr, const array &y_arr)
+    {
+        if (x_arr.n != y_arr.n)
+        {
+            throw std::runtime_error("Length of arrays should match");
+        }
+
+        auto len = x_arr.n;
+        auto result = array(len); // empty result array
+        auto result_ptr = result.storage.get();
+        auto simd_iter = (len / 4);
+
+        for (size_t i = 0; i < simd_iter; i += 4)
+        {
+            auto x_ptr = x_arr.storage.get();
+            auto y_ptr = y_arr.storage.get();
+            auto x_vec = __m256d{x_ptr[i + 0], x_ptr[i + 1], x_ptr[i + 2], x_ptr[i + 3]};
+            auto y_vec = __m256d{y_ptr[i + 0], y_ptr[i + 1], y_ptr[i + 2], y_ptr[i + 3]};
+            auto mul = _mm256_mul_pd(x_vec, y_vec);
+            result_ptr[i + 0] = mul[0];
+            result_ptr[i + 1] = mul[1];
+            result_ptr[i + 2] = mul[2];
+            result_ptr[i + 3] = mul[3];
+        }
+
+        for (size_t i = 0; i < len % 4; i++)
+        {
+            auto x_ptr = x_arr.storage.get();
+            auto y_ptr = y_arr.storage.get();
+            auto offset = simd_iter * 4;
+            result_ptr[offset + i] = x_ptr[offset + i] * y_ptr[offset + i];
+        }
+
+        return result;
+    }
 
 }
--- cpu/impl.cpp
+++ cpu/impl.cpp
@@ -3,15 +3,22 @@
 namespace cpu
 {
 
-    std::array<double, 4> mul(const std::array<double, 4> &x, const std::array<double, 4> &y)
+    array mul(const array &x_arr, const array &y_arr)
     {
-        std::array<double, 4> result;
-        for (size_t sz = 0; sz < x.size(); ++sz)
+        if (x_arr.n != y_arr.n)
         {
-            result[sz] = x[sz] * y[sz];
+            throw std::runtime_error("Length of arrays should match");
         }
-        std::cout << "NORMAL IMPL"
-                  << "\n";
+
+        auto len = x_arr.n;
+        auto result = array(len); // empty result array
+        auto result_ptr = result.storage.get();
+        for (size_t sz = 0; sz < x_arr.n; ++sz)
+        {
+            auto x_ptr = x_arr.storage.get();
+            auto y_ptr = y_arr.storage.get();
+            result_ptr[sz] = x_ptr[sz] * y_ptr[sz];
+        }
         return result;
     }
 
